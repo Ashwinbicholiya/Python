80984fe9a2bbf8a3ca1eeb8f2fee317cd97bcfb9
Ashwinbicholiya <ashwinbicholiya@gmail.com>
2020-05-09 20:07:08 +0530

    commit

---
+++ 28ones6.py
@@ -0,0 +1,6 @@
+from numpy import *
+#it will create a array of 5 with all the value is 1 
+# Default typw is float
+#if you want to change the type ,simply type int after the length of array that is 5 
+arr=ones(5,int)
+print(arr)
---
+++ 28zeroes5.py
@@ -0,0 +1,5 @@
+from numpy import *
+#it will create a array of 5 with all the  value is 0
+#default type is float
+arr=zeros(5)
+print(arr)
