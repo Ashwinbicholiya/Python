8d66001cc1c786e8974cc93623289edb13bbb634
Ashwinbicholiya <ashwinbicholiya@gmail.com>
2020-06-16 21:58:40 +0530

    commit

---
+++ 21DSA/Linear_search.py
@@ -0,0 +1,16 @@
+pos = 1
+def Search(list, n):
+    for i in range(len(list)):
+        if list[i] ==n:
+            globals()['pos'] = i
+            return True 
+    return False
+
+list = [8,4,6,7,4]
+n = 7
+
+if Search(list, n):
+    print('Found at ',pos+1)
+else:
+    print('Not Found')
+
