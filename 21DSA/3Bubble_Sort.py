0de76e0318bf6bbddcd3f3e7e2b6bdb3081bb658
Ashwinbicholiya <ashwinbicholiya@gmail.com>
2020-06-19 20:16:14 +0530

    commit

--- 21DSA/3Bubble_Sort.py
+++ 21DSA/3Bubble_Sort.py
@@ -1,12 +1,16 @@
-nums = [8,5,14,42,11,45]
+def sort(nums):
+    for i in range(len(nums)-1, 0 ,-1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+nums = []
+n = int(input('Enter the length of value '))
 
-for i in range(len(nums)-1,0,-1):
-    for j in range(i):
-        if nums[j] > nums[j+1]:
-            temp = nums[j]
-            nums[j] = nums[j+1]
-            nums[j+1] = temp
+for e in range(n):
+    val = int(input('Enter the values : '))
+    nums.append(val)
 
+sort(nums)
 print(nums)
-
- 
