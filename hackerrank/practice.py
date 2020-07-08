0175ff69eb008d601f78964c0f11cce7bf4fab15
Ashwinbicholiya <ashwinbicholiya@gmail.com>
2020-07-08 18:31:43 +0530

    commit

--- hackerrank/practice.py
+++ hackerrank/practice.py
@@ -1,3 +1,9 @@
 import sys
 sys.stdout = open('hackerrank/output.txt', 'w')
 sys.stdin = open('hackerrank/input.txt', 'r')
+from itertools import permutations
+
+s ,k =  input().split()
+p = list(permutations(sorted(s),int(k)))
+for i in p:
+    print(''.join(i))
