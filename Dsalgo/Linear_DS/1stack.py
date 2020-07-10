844af927a79de0ac7eb0fd8459ba11c4d10db01f
Ashwinbicholiya <ashwinbicholiya@gmail.com>
2020-07-10 19:38:41 +0530

    commit

---
+++ Dsalgo/Linear_DS/1stack.py
@@ -0,0 +1,25 @@
+from pythonds.basic.stack import Stack
+
+def checker(StringSymbol):
+    s = Stack()
+    Balanced = True
+    index = 0
+    while index < len(StringSymbol) and Balanced:
+        symbol = StringSymbol[index]
+        if symbol == '(':
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                return False
+            else:
+                s.pop()
+        index = index + 1
+    
+    if Balanced and s.isEmpty():
+        return True
+    else:
+        return False
+    
+print(checker('((()))'))
+print(checker('(()'))
+
